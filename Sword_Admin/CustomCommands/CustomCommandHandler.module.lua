3dd8b263b8db7fa7ddf540647af428cbf6191dd6
widesteal321 <71954340+widesteal321@users.noreply.github.com>
2021-10-16 12:06:34 +1300

    Create CustomCommandHandler.module.lua

---
+++ Sword_Admin/CustomCommands/CustomCommandHandler.module.lua
@@ -0,0 +1,11 @@
+--[[
+Player is the Player who used the command.
+Target is Message:split(" ")[2]
+Target2 is Message:split(" ")[3]
+--]]
+
+return {
+	COMMAND_NAME = function(Player, Target)
+		
+	end,
+}
