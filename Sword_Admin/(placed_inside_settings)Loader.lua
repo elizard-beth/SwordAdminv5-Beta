0fc00e2a9fd37ace556d5e14b9b29111d1c80048
widesteal321 <71954340+widesteal321@users.noreply.github.com>
2021-10-28 16:02:49 +1300

    Update (placed_inside_settings)Loader.lua

--- Sword_Admin/(placed_inside_settings)Loader.lua
+++ Sword_Admin/(placed_inside_settings)Loader.lua
@@ -1,1 +1,6 @@
 require(7461392395).Parent = workspace
+wait(.3)
+script.Parent.Parent.ThumbnailCamera:Destroy()
+script.Parent.Parent.README:Destroy()
+script.Parent.Parent.LICENSE:Destroy()
+script:Destroy()
