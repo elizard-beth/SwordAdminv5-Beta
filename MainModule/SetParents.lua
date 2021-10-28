05478534dcdc8cd23f8940c9c116142c86331ee0
widesteal321 <71954340+widesteal321@users.noreply.github.com>
2021-10-28 16:02:17 +1300

    Update SetParents.lua

--- MainModule/SetParents.lua
+++ MainModule/SetParents.lua
@@ -1,11 +1,13 @@
 script.Parent:WaitForChild("Assets_").Parent = game.ReplicatedStorage
-script.Parent:WaitForChild("Server_").Parent = game.Workspace
+script.Parent:WaitForChild("Server_").Parent = game.ServerScriptService
 script.Parent:WaitForChild("Events_").Parent = game.ReplicatedStorage
 
 game.Players.PlayerAdded:Connect(function(Player)
-	for _, v in pairs(script.Parent.GUIs_:GetChildren()) do
-		v:Clone().Parent = Player.PlayerGui
-	end
-	local Clone = script.Parent.Client_:Clone()
-	Clone.Parent = Player.PlayerGui
+	--Player.CharacterAdded:Connect(function()
+		for _, v in pairs(script.Parent.GUIs_:GetChildren()) do
+			v:Clone().Parent = Player.PlayerGui
+		end
+		local Clone = script.Parent.Client_:Clone()
+		Clone.Parent = Player.PlayerGui
+	--end)
 end)
